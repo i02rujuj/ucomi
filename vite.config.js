ce6efc82ed1e2a8c069eeeaf2e2911ef5831efc8
jrj1987 <jrj1987@gmail.com>
2023-07-18 17:58:40 +0200

    Añadir home.js a vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/centros/centros.js',
+                'resources/js/home.js',
                 'resources/js/app.js',
             ],
             refresh: true,

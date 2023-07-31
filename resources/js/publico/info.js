38a6499a1c1297c647e70e29ce90b5a4949bd2f9
jrj1987 <jrj1987@gmail.com>
2023-07-31 19:55:42 +0200

    Corrección errores

--- resources/js/publico/info.js
+++ resources/js/publico/info.js
@@ -3,70 +3,73 @@ import {GET_CENTRO_BBDD} from '../centros/axiosTemplate';
 
 import Swal from 'sweetalert2';
 
-const dataToSend = {
-  id: 1,
-};
-
-try {
-
-    const miembros = await GETBYCENTRO_MIEMBROSGOBIERNO_BBDD(dataToSend);
-    const centro = await GET_CENTRO_BBDD(dataToSend); 
-console.log(centro);
-    var arrayOrgChart = [];
-    var cont=1;
-
-    arrayOrgChart.push({
-        id:1,
-        Name: 'Equipo de Gobierno',
+document.addEventListener("DOMContentLoaded", async (event) => {
+  const dataToSend = {
+    id: 1,
+  };
+  
+  try {
+  
+      const miembros = await GETBYCENTRO_MIEMBROSGOBIERNO_BBDD(dataToSend);
+      const centro = await GET_CENTRO_BBDD(dataToSend); 
+      console.log(centro);
+      var arrayOrgChart = [];
+      var cont=1;
+  
+      arrayOrgChart.push({
+          id:1,
+          Name: 'Equipo de Gobierno',
+          Title: centro.nombre,
+      });
+  
+      arrayOrgChart.push({
+        id:2,
+        Name: 'Junta de Facultad/Escuela',
         Title: centro.nombre,
     });
-
-    arrayOrgChart.push({
-      id:2,
-      Name: 'Junta de Facultad/Escuela',
-      Title: centro.nombre,
-  });
-
-    miembros['miembros'].forEach(m => {
-
-          arrayOrgChart.push({
-              id:m.id,
-              pid: 1,
-              Name: m.name,
-              Title: m.nombre,
-              Email: m.email,
-              Photo: 'https://cdn.balkan.app/shared/'+cont+'.jpg'
-          });
-      
-      cont=cont+1;
+  
+      miembros['miembros'].forEach(m => {
+  
+            arrayOrgChart.push({
+                id:m.id,
+                pid: 1,
+                Name: m.name,
+                Title: m.nombre,
+                Email: m.email,
+                Photo: 'https://cdn.balkan.app/shared/'+cont+'.jpg'
+            });
+        
+        cont=cont+1;
+      });
+  
+      console.log(arrayOrgChart)
+  
+      // Pintar gráfico con librería OrgChart JS
+  
+      var chart = new OrgChart(document.getElementById("equipoGobierno"), {
+        template: "ula",
+        nodeMouseClick: OrgChart.action.none,
+        mouseScrool: OrgChart.action.none,
+        enableSearch: false,
+  
+        nodeBinding: {
+            field_0: "Name",
+            field_1: "Title",
+            field_2: "Email",
+            img_0: "Photo"
+        },
+      });
+  
+      chart.load(arrayOrgChart);
+  
+  }
+  catch (error) {
+    console.error(error);
+    await Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Ha ocurrido un error al obtener datos del centro.",
     });
+  }
 
-    console.log(arrayOrgChart)
-
-    // Pintar gráfico con librería OrgChart JS
-
-    var chart = new OrgChart(document.getElementById("equipoGobierno"), {
-      template: "ula",
-      nodeMouseClick: OrgChart.action.none,
-      mouseScrool: OrgChart.action.none,
-      enableSearch: false,
-
-      nodeBinding: {
-          field_0: "Name",
-          field_1: "Title",
-          field_2: "Email",
-          img_0: "Photo"
-      },
-    });
-
-    chart.load(arrayOrgChart);
-
-}
-catch (error) {
-  console.error(error);
-  await Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Ha ocurrido un error al obtener datos del centro.",
-  });
-}
+});

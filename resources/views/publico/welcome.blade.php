88c587d56309a71b92334c8de08f92fc6d9f7aa2
jrj1987 <jrj1987@gmail.com>
2024-07-23 02:18:42 +0200

    Solucionar error tipo en welcome

--- resources/views/publico/welcome.blade.php
+++ resources/views/publico/welcome.blade.php
@@ -20,7 +20,7 @@ Ucomi
                             @foreach ($centros as $centro)
                                 <button type="submit" name="centro" value="{{$centro->id}}">
                                     <div class='contenedor tooltip'>
-                                        <img src="{{ $centro->logo ? $centro->logo : asset('img/default_image.png') }}" alt="{{$centro->tipo != config('constants.TIPOS_CENTRO.OTRO') ? $centro->tipo->nombre : ''}} {{$centro->nombre}}" class="bg-white w-24 h-24 ml-1 mb-1 object-cover logo rounded-md">   
+                                        <img src="{{ $centro->logo ? $centro->logo : asset('img/default_image.png') }}" alt="{{$centro->tipo->id != config('constants.TIPOS_CENTRO.OTRO') ? $centro->tipo->nombre : ''}} {{$centro->nombre}}" class="bg-white w-24 h-24 ml-1 mb-1 object-cover logo rounded-md">   
                                         <span class="tooltiptext">{{$centro->tipo->id != config('constants.TIPOS_CENTRO.OTRO') ? $centro->tipo->nombre : ''}} {{$centro->nombre}}</span>
                                     </div>
                                 </button>                      

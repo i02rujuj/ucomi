74ed9071ac47614b8d0fb3ee18cdff1b9f47747c
jrj1987 <jrj1987@gmail.com>
2024-07-24 02:07:30 +0200

    acta info abrir en nueva pestaña

--- resources/js/publico/info.js
+++ resources/js/publico/info.js
@@ -2,15 +2,16 @@ import Swal from 'sweetalert2';
 
 const showButton = document.querySelector('#btn-show-acta');
 showButton.addEventListener("click", async (event) => {
-    await Swal.fire({
+    window.open(showButton.dataset.acta, '_blank');
+    
+    /*await Swal.fire({
         html: `
             <iframe src="${showButton.dataset.acta}" class="w-full h-full"></iframe>
         `,
         focusConfirm: false,
         showConfirmButton: false,
-        showCloseButton: true,
         width: '90vw',
         heightAuto:false, 
         customClass: 'swal-height'   
-    })
+    })*/
 })
